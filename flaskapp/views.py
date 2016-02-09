4371b54001d2302449cb00fa5c0c1e1b02f6c43b
kurohai <kurohai@gmail.com>
2016-02-09 12:12:17 -0600

    voice change

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, send_file
 from dicto import dicto
 from sqlalchemy import and_
 from pprint import pprint

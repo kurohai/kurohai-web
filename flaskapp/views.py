339b0b18b972735420855ce2e811cb557e518e13
Evan <kurohai@gmail.com>
2016-09-01 19:46:51 -0500

    added wiki view

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect
-from flask import render_template, request, url_for, send_file
+from flask import render_template, request, url_for, send_file, send_from_directory
 from dicto import dicto
 from sqlalchemy import and_
 from pprint import pprint
@@ -14,18 +14,33 @@ blueprint = Blueprint(flasktemplate.appnamed, __name__)
 def poetry():
     return render_template('public/poetry.html')
 
+
 @blueprint.route('/')
 def home():
     return render_template('public/index.html')
+
 
 @blueprint.route('/voice.xml', methods=['GET', 'POST'])
 def voice():
     return send_file('templates/voice.xml')
 
-@blueprint.route('/colord/')
-def colord():
-    return redirect('http://colord.kurohai.com/', code=302)
 
 @blueprint.route('/resume/python-dev/')
 def python_dev():
     return render_template('public/resume/python-dev.html')
+
+
+@blueprint.route('/wiki/<path:path>')
+def wiki(path):
+    print path
+    fullpath = os.path.abspath(os.curdir) + '/flaskapp/templates/wiki/'
+    if os.path.isdir(os.path.join(fullpath, path)):
+        path = path + 'index.html'
+        print 'last one:', fullpath
+
+    return send_from_directory(fullpath, path)
+
+
+@blueprint.route('/wiki/')
+def wiki_home():
+    return wiki('index.html')

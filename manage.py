f6c5534bcbfd993e5dc512f4310da54f0b25eb5e
Evan <kurohai@gmail.com>
2016-09-01 23:06:08 -0500

    more logging

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ def db():
 @manager.command
 def quick():
     # d = wsgiserver.WSGIPathInfoDispatcher({'/kurohai': flasktemplate, '/colord': colord_flasktemplate})
-    d = wsgiserver.WSGIPathInfoDispatcher({'/kurohai': flasktemplate})
+    d = wsgiserver.WSGIPathInfoDispatcher({'/': flasktemplate})
     server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 80), d, server_name=flasktemplate.appname, )
     try:
         server.start()
